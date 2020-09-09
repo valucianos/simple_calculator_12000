4f55e20c14fa19d1b602b694ad3f3cc5f2b99496
valucianos <70271776+valucianos@users.noreply.github.com>
2020-09-09 11:11:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var calculatorEle = document.body.querySelector(".calculator");
+var number1 = Number(prompt("Enter a number."));
+if(isNaN(number1)){
+   calculatorEle.innerHTML="Value entered for number 1 is not valid.";
+}
+var number2 = Number(prompt("Enter second number."));
+if(isNaN(number2)){
+   calculatorEle.innerHTML="Value entered for number 2 is not valid.";
+}
+var operator=prompt("Enter an operation to be performed.")
+if(operator=="*"){
+var total= number1*number2;
+  document.body.querySelector(".calculator").innerHTML=total;
+}else if(operator=="/"){
+  var total=number1/number2;
+  document.body.querySelector(".calculator").innerHTML=total;
+}else if(operator=="+"){
+  var total = number1+number2;
+  document.body.querySelector(".calculator").innerHTML=total;
+}else if(operator=="-"){
+  var total=number1-number2;
+  document.body.querySelector(".calculator").innerHTML=total;
+}else{
+  calculatorEle.innerHTML="Operator entered not valid.";
+}
